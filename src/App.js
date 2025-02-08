9e4955b176ebdae447c368875bfec9da2aa523bd
ManiMindScripts <ar7279796@gmail.com>
2025-02-08 06:00:48 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Product from './Pages/Product'
 import LoginSignup from './Pages/LoginSignup'
 import Card from './Pages/Card'
 import Footer from './Components/Footer/Footer'
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
+
 
 export default function App() {
   return (
@@ -17,9 +21,9 @@ export default function App() {
       <Navbar/>
         <Routes>
             <Route path='/' element={<Shop/>}/>
-            <Route path='/mens' element={<ShopCategory category="men"/>}/>
-            <Route path='/womens' element={<ShopCategory category="women"/>}/>
-            <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+            <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+            <Route path='/womens' element={<ShopCategory banner= {women_banner} category="women"/>}/>
+            <Route path='/kids' element={<ShopCategory banner ={kids_banner} category="kid"/>}/>
             <Route path='/product' element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
             </Route>
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Css/Signup.css'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='signup'>
+      <div className='signup-container'>
+        <h1>Sign Up</h1>
+        <div className='signup-fields'>
+          <input type='text' placeholder='Your Name' />
+          <input type='email' placeholder='Your Email Id' />
+          <input type='password' placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='sign'>Already have an account? <span>login Here</span></p>
+        <div className='signup-agree'>
+          <input type='checkbox' name='' id='' />
+          <p>By Continuing, i agree the terms of use & Policy </p>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
